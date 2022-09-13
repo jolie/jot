733acf838254c3baaaf2270b118e68571dec1acb
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-09-14 00:17:00 +0200

    jot sh and resolving symbolic link in jot script wrapper

--- jot.sh
+++ jot.sh
@@ -42,4 +42,12 @@ if [[ $PARAMS == "" ]]; then
     exit 1;
 fi
 
-/usr/bin/env -S jolie --params $PARAMS ./jot.ol ${SCRIPT_PARAMS[@]}
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+/usr/bin/env -S jolie --params $PARAMS $DIR/jot.ol ${SCRIPT_PARAMS[@]}
